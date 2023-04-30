4e0d29b1ff902f073aa17c0ea0019879b907d7d5
YotaMatsushima <yota.surf@gmail.com>
2023-04-30 14:04:16 +0900

    Update youtube2sum.py

--- youtube/youtube2sum.py
+++ youtube/youtube2sum.py
@@ -1,9 +1,11 @@
 import os
 import re
+import argparse
 
 import dotenv
 import openai
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import NoTranscriptFound
 
 # Load environment variables
 dotenv.load_dotenv()
@@ -16,18 +18,23 @@ openai.organization = openai_organization
 
 def main():
     """Main function to get and process the subtitles, and save the summaries."""
-    # Specify the Youtube video URL
-    youtube_url = input("Youtube URL:")
+    # Create argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("youtube_url", help="YouTube video URL.")
+    parser.add_argument("language_code", help="Language code for the subtitles.")
+    args = parser.parse_args()
 
     # Extract video id from url
-    video_id = re.findall(r'(?:v=|\/)([0-9A-Za-z_-]{11}).*', youtube_url)[0]
+    video_id = re.findall(r'(?:v=|\/)([0-9A-Za-z_-]{11}).*', args.youtube_url)[0]
 
     print("字幕を読み込み中\n")
-
-    # Get the subtitles of the video
-    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-    transcript = transcript_list.find_transcript(['ja'])
-    subtitles = transcript.fetch()
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript = transcript_list.find_transcript([args.language_code])
+        subtitles = transcript.fetch()
+    except NoTranscriptFound:
+        print("エラー：指定した言語の字幕が見つかりませんでした。")
+        return
 
     # Collect subtitles without timestamps
     text = ""
@@ -54,12 +61,20 @@ def main():
             print(chunk)
             response = openai.Completion.create(
                 engine="text-davinci-003",
-                prompt="日本語で議論の概要とポイントをmarkdown方式でまとめる:summary so far["+summary+"] 1additional text: "+chunk,
+                prompt="議論の概要と議論のポイントをまとめて:summary so far["+summary+"] additional text: "+chunk,
                 temperature=0.3,
                 max_tokens=4000
             )
             summary = response.choices[0].text.strip()
 
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt="日本語で議論の概要と議論のポイントをmarkdown方式でまとめて:"+summary,
+            temperature=0.3,
+            max_tokens=4000
+        )
+
+        summary = response.choices[0].text.strip()
         out_file.write(summary + '\n\n')
 
 if __name__ == "__main__":
